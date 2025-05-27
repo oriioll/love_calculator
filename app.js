ca421670dba896f98911cc4fd67be2cbc2e8f8ae
oriioll <oriolplazas2@gmail.com>
2025-05-27 23:40:57 +0200

    Primer commit - iniciando repositorio

---
+++ app.js
@@ -0,0 +1,38 @@
+const name1 = document.getElementById('name1');
+const name2 = document.getElementById('name2');
+let score = 0;
+
+function lowername(nombre) {
+    return nombre.toLowerCase().trim(); // También quitamos espacios en blanco por si acaso
+}
+
+document.getElementById('calculate').addEventListener('click', function() {
+    const name1lower = lowername(name1.value);
+    const name2lower = lowername(name2.value);
+
+    if (name1.value === '' || name2.value === '') {
+        alert('Please enter both names.');
+        return;
+    } else if (
+        (name1lower === 'oriol' && name2lower === 'gemma') || (name1lower === 'gemma' && name2lower === 'oriol')
+    ) {
+        score = 100;
+        document.getElementById('show').innerText = `${score}%`;
+    } else {
+        score = 0;
+        document.getElementById('show').innerText = `${score}%`;
+    }
+});
+
+
+let openModal = false
+
+document.getElementById('modal').addEventListener('click', function() {
+    if (openModal === false) {
+        document.querySelector('.instrucciones').style.display = 'block';
+        openModal = true;
+    } else {
+        document.querySelector('.instrucciones').style.display = 'none';
+        openModal = false;
+    }
+});
